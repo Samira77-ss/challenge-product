5df3051f3bdccb92d420591ed37149baf60bdd38
Samira77-ss <samiraseyfii0709@gmail.com>
2023-10-07 12:10:11 +0330

    Added react structure

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="image-product.jpg"
+              className="img-fluid"
+              alt="perfume-product"
+            />
+          </div>
+          <div className="col-6"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
